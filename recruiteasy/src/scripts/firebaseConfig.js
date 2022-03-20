d6ac5f7a2b54175d7e3f03c3d01d66c4b3a63bde
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2022-03-20 18:00:22 +0530

    bug fix

---
+++ recruiteasy/src/scripts/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-AbSeCrXW_Xm764vKIq1sSr9IPiyo9oE",
+    authDomain: "recruiteasy-ed25f.firebaseapp.com",
+    projectId: "recruiteasy-ed25f",
+    storageBucket: "recruiteasy-ed25f.appspot.com",
+    messagingSenderId: "980550520151",
+    appId: "1:980550520151:web:ae10c284ec592397f90212"
+  }

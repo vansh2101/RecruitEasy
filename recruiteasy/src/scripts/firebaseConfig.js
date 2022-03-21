1f1a21f178e2e13ae8ec354acbc9a0c48d8d4d09
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2022-03-21 11:49:10 +0530

    removed api key

--- recruiteasy/src/scripts/firebaseConfig.js
+++ recruiteasy/src/scripts/firebaseConfig.js
@@ -1,10 +1,8 @@
-// require('dotenv').config()
-
 export const firebaseConfig = {
-    apiKey: 'AIzaSyB-AbSeCrXW_Xm764vKIq1sSr9IPiyo9oE',
-    authDomain: "recruiteasy-ed25f.firebaseapp.com",
-    projectId: "recruiteasy-ed25f",
-    storageBucket: "recruiteasy-ed25f.appspot.com",
-    messagingSenderId: "980550520151",
-    appId: "1:980550520151:web:ae10c284ec592397f90212"
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_DOMAIN,
+    projectId: process.env.REACT_APP_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGING,
+    appId: process.env.REACT_APP_APP
   }
